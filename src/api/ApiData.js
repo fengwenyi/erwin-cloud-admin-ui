7387d4589976b36af9fa3d3d3275a03636f5f654
冯文议 <xfsy_2015@163.com>
2020-04-27 22:02:13 +0800

    init

---
+++ src/api/ApiData.js
@@ -0,0 +1,137 @@
+import request from '@/libs/request'
+
+// 登录
+export const login = (username, password) => {
+  const data = {
+    username,
+    password
+  }
+  return request({
+    url: '/api-index/auth/login',
+    method: 'post',
+    data: data
+  })
+}
+
+// 退出登录
+export const logout = () => {
+  const data = {}
+  return request({
+    url: '/api-user/auth/logout',
+    method: 'get',
+    data: data
+  })
+}
+
+//----- 角色 ---------------------------------------------------------------
+
+// 添加角色
+export const apiRoleAdd = (data) => {
+  return request({
+    url: '/api-admin/role',
+    method: 'post',
+    data: data
+  })
+}
+
+// 角色列表
+export const apiRoleGet = (data) => {
+  return request({
+    url: '/api-admin/role',
+    method: 'get',
+    data: {},
+    params: data
+  })
+}
+
+// 角色 修改发布状态
+export const apiRoleUpdateRelease = (id, data) => {
+  return request({
+    url: '/api-admin/role/' + id + '/release',
+    method: 'put',
+    data: {},
+    params: data
+  })
+}
+
+// 角色 修改
+export const apiRoleUpdate = (id, data) => {
+  return request({
+    url: '/api-admin/role/' + id,
+    method: 'patch',
+    data: data
+  })
+}
+
+// 角色 逻辑删除
+export const apiRoleDelete = (id) => {
+  return request({
+    url: '/api-admin/role/' + id,
+    method: 'delete',
+    data: {}
+  })
+}
+
+//----- 用户 ---------------------------------------------------------------
+
+// 用户列表
+export const apiUserGet = (data) => {
+  return request({
+    url: '/api-admin/user',
+    method: 'get',
+    data: {},
+    params: data
+  })
+}
+
+// 用户 修改发布状态
+export const apiUserUpdateReleaseStatus = (id, data) => {
+  return request({
+    url: '/api-admin/user/' + id + '/release',
+    method: 'put',
+    data: {},
+    params: data
+  })
+}
+
+// 用户 修改锁定状态
+export const apiUserUpdateLockStatus = (id, data) => {
+  return request({
+    url: '/api-admin/user/' + id + '/lock',
+    method: 'put',
+    data: {},
+    params: data
+  })
+}
+
+// 用户 修改
+export const apiUserUpdate = (id, data) => {
+  return request({
+    url: '/api-admin/user/' + id,
+    method: 'patch',
+    data: data
+  })
+}
+
+// 用户 逻辑删除
+export const apiUserDelete = (id) => {
+  return request({
+    url: '/api-admin/user/' + id,
+    method: 'delete',
+    data: {}
+  })
+}
+
+//----- 博客系统 start ---------------------------------------------------------------
+
+// 类目：分页查询列表
+export const apiBlogSystemCategoryGetPageList = (currentPage) => {
+  return request({
+    url: '/erwin-cloud-blog/api-admin/category/page/' + currentPage,
+    method: 'get',
+    data: {}
+  })
+}
+
+
+//----- 博客系统 end -----------------------------------------------------------------
---
+++ src/view/blog-system/category-management/CategoryManagement.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        类目管理
+    </div>
+</template>
+
+<script>
+    // import { apiBlogSystemCategoryGetPageList } from "@/api/ApiData"
+
+	export default {
+		name: "CategoryManagement",
+        data () {
+		    return {
+		    	page: {
+		    		currentPage: 1
+                }
+            }
+        },
+        methods: {
+			getData() {
+				// apiBlogSystemCategoryGetPageList(this.page.currentPage).then(result => {
+				// 	console.log(result)
+                // })
+            }
+        },
+        mounted() {
+			this.getData()
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
